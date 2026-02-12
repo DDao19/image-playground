3d123d28719e94669a72b52ac1b12f647f99bce9
ddao19 <dung.dao19@outlook.com>
2026-02-12 17:53:28 -0600

    Updated code to convert JPG images to PNG

--- images.py
+++ images.py
@@ -1,7 +1,1 @@
-from PIL import Image, ImageFilter
-
-img = Image.open("./Pokedex/pikachu.jpg")
-filteredImg = img.convert("L")
-rotatedImg = filteredImg.rotate(90)
-
-rotatedImg.save("rotate.png", "png")
+This is some text
