438f7573ed8419453b45b8729cee15ed1c959566
ddao19 <dung.dao19@outlook.com>
2026-02-13 15:20:28 -0600

    Completed converter mini project

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,6 +1,6 @@
+import sys
 from PIL import Image
 from pathlib import Path
-import sys
 
 user_params = sys.argv[1::]
 if len(user_params) > 1:
@@ -10,20 +10,25 @@ if len(user_params) > 1:
     # Grab all image files
     image_files = list(Path(f"{parent_folder}").glob("*.jpg"))
 
-    # Create sub folder in Pokedex called New
+    # Create sub folder in Pokedex called 'New'
     sub_folder = Path(f"{parent_folder}/{new_folder}")
 
     try:
         if parent_folder.exists():
-            sub_folder.mkdir(exist_ok=True)
+            if not sub_folder.exists():
+                sub_folder.mkdir()
+                for image in image_files:
+                    image_file = Image.open(f"{parent_folder}/{image.name}")
 
-            for image in image_files:
-                image_file = Image.open(f"{parent_folder}/{image.name}")
+                    new_filename = image.stem + ".png"
+                    save_path = sub_folder / new_filename
 
-                new_filename = image.stem + ".png"
-                save_path = sub_folder / new_filename
+                    image_file.save(save_path)
+                print(f"Folder: '{new_folder}' has been created!")
 
-                image_file.save(save_path)
+            else:
+                print(f"Sorry, folder: '{new_folder}' already exists.")
+
         else:
             print("Sorry, that Parent directory does not exist")
     except Exception as err:
