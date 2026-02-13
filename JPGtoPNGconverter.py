3c48ab3c9e2f6f303c92e724c51fcd8a5a468aa7
ddao19 <dung.dao19@outlook.com>
2026-02-13 11:40:54 -0600

    Added conditional to enter 2 parameters

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -3,28 +3,31 @@ from pathlib import Path
 import sys
 
 user_params = sys.argv[1::]
-parent_folder = Path(user_params[0])
-new_folder = Path(user_params[1])
+if len(user_params) > 1:
+    parent_folder = Path(user_params[0])
+    new_folder = Path(user_params[1])
 
-# Grab all image files
-image_files = list(Path(f"{parent_folder}").glob("*.jpg"))
+    # Grab all image files
+    image_files = list(Path(f"{parent_folder}").glob("*.jpg"))
 
-# Create sub folder in Pokedex called New
-sub_folder = Path(f"{parent_folder}/{new_folder}")
+    # Create sub folder in Pokedex called New
+    sub_folder = Path(f"{parent_folder}/{new_folder}")
 
+    try:
+        if parent_folder.exists():
+            sub_folder.mkdir(exist_ok=True)
 
-try:
-    if parent_folder.exists():
-        sub_folder.mkdir(exist_ok=True)
+            for image in image_files:
+                image_file = Image.open(f"{parent_folder}/{image.name}")
 
-        for image in image_files:
-            image_file = Image.open(f"{parent_folder}/{image.name}")
+                new_filename = image.stem + ".png"
+                save_path = sub_folder / new_filename
 
-            new_filename = image.stem + ".png"
-            save_path = sub_folder / new_filename
+                image_file.save(save_path)
+        else:
+            print("Sorry, that Parent directory does not exist")
+    except Exception as err:
+        print(f"Something went wrong {err}")
 
-            image_file.save(save_path)
-    else:
-        print("Sorry, that Parent directory does not exist")
-except Exception as err:
-    print(f"Something went wrong {err}")
+else:
+    print("Error: Must enter 2 parameters")
